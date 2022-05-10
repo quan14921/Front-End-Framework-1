0a0b9efd2a260f1d509ad5d965fbadc1cb2f3454
quanth <quanthph14921@fpt.edu.vn>
2022-05-10 15:47:17 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './component/xyz/xyz.component';
+import { ProductComponent } from './component/product/product.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    ProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
